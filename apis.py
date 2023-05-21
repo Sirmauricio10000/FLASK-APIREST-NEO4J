4f120d6e6efbb23310ffd2cd346f7ee8897be326
Sirmauricio10000 <mauricio1907_5@outlook.com>
2023-05-21 16:20:31 -0500

    endpoints added

--- apis.py
+++ apis.py
@@ -16,14 +16,19 @@ class NodesResource(Resource):
         response = get_all_nodes()
         return response
 
-
 @namespace.route('/rutas/ruta_mas_corta/<string:origen>/<string:destino>')
 class RutaResources(Resource):
     @api.doc(description='Obtiene los caminos directos más cortos de una ruta en base a un origen y destino')
     def get(self, origen, destino):
        response = get_ruta_mas_corta(origen, destino)
        return jsonify(response)
-    
+
+@namespace.route('/rutas/rutas_de_un_nodo/<string:nodo>')
+class RutaResources(Resource):
+    @api.doc(description='Obtiene todas las rutas de un nodo')
+    def get(self, nodo):
+       response = get_rutas_de_un_nodo(nodo)
+       return jsonify(response)
 
 @namespace.route('/rutas/ruta_individual/<string:ruta>')
 class RutaResource(Resource):
--- validation.py
+++ validation.py
@@ -7,33 +7,40 @@ api = Api()
 
 
 def get_all_nodes():
-    response = graph.run("MATCH (n) RETURN n").data()
+    response = graph.run("""MATCH (n)
+        RETURN collect(n.nombre) AS nodos""").data()
     return response
 
 def get_route(ruta):
-    query = "MATCH (n:Parada)-[r:" + ruta + "]->(m:Parada) RETURN collect(n.nombre) AS camino"
-    result = graph.run(query).data()
-    response = {
-        'nodes': result
-    }
+    query = """MATCH (n:Parada)-[r:""" + ruta + """]->(m:Parada)
+        WITH COLLECT(n) + COLLECT(m) AS nodos, r
+        UNWIND nodos AS nodo
+        WITH collect(DISTINCT CASE WHEN r.tipo = "ida" THEN nodo.nombre ELSE null END) AS ida, collect(DISTINCT CASE WHEN r.tipo = "vuelta" THEN nodo.nombre ELSE null END) AS vuelta
+        RETURN { ida: ida, vuelta: vuelta } AS camino"""
+    response = graph.run(query).data()
     return response
+
+def get_rutas_de_un_nodo(nodo):
+    query = """ MATCH (n:Parada {nombre: '""" +nodo+ """'})-[r]-(m:Parada)
+        WITH collect({coste: r.coste, ruta: type(r), tipo: r.tipo}) AS rutas
+        RETURN rutas"""
+    result = graph.run(query).data()
+    return result
 
 def get_ruta_mas_corta(origen, destino):
     query = """MATCH (origen:Parada {nombre: $origen}), (destino:Parada {nombre: $destino})
-            WITH origen, destino, ['ruta_100', 'ruta_101', 'ruta_214', 'ruta_313', 'ruta_316', 'ruta_561', 'ruta_562'] AS rutas
-            UNWIND rutas AS ruta
-            CALL apoc.cypher.run('
-            MATCH path = (o:Parada {nombre: $origen})-[*1..50]->(d:Parada {nombre: $destino})
-            WHERE ALL(rel in relationships(path) WHERE type(rel) = $ruta)
-            RETURN [node in nodes(path) | node.nombre] AS camino, $ruta AS ruta, length(path) AS longitud
-            ORDER BY longitud
-            LIMIT 1
-            ', {origen: origen.nombre, destino: destino.nombre, ruta: ruta}) YIELD value
-            RETURN value.camino AS camino, value.ruta AS ruta, value.longitud AS longitud"""
+        WITH origen, destino, ['ruta_100', 'ruta_101', 'ruta_214', 'ruta_313', 'ruta_316', 'ruta_561', 'ruta_562'] AS rutas
+        UNWIND rutas AS ruta
+        CALL apoc.cypher.run('
+        MATCH path = (o:Parada {nombre: $origen})-[*1..50]->(d:Parada {nombre: $destino})
+        WHERE ALL(rel in relationships(path) WHERE type(rel) = $ruta)
+        RETURN [node in nodes(path) | node.nombre] AS camino, $ruta AS ruta, length(path) AS longitud
+        ORDER BY longitud
+        LIMIT 1
+        ', {origen: origen.nombre, destino: destino.nombre, ruta: ruta}) YIELD value
+        RETURN value.camino AS camino, value.ruta AS ruta, value.longitud AS longitud"""
         
     result = graph.run(query, origen=origen, destino=destino).data()
-
-    print(result)
     
     resultados_procesados = []
     
