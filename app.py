fbb7e086fb980ab70f8e7c506638921f5776985c
Sirmauricio10000 <mauricio1907_5@outlook.com>
2023-05-23 16:22:29 -0500

    Manejo de excepciones

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, redirect, url_for
 from flask_restx import Api, Resource
 from connection import get_all_nodes, get_ruta_mas_corta, get_rutas_de_un_nodo, get_route
+import requests
 
 
 app = Flask(__name__)
@@ -16,29 +17,32 @@ def render_swagger():
 class AllNodes(Resource):
     @api.doc(description='Obtiene todas las Paradas')
     def get(self):
-        response = get_all_nodes()
-        return response
+        response, status_code = get_all_nodes()
+        return response, status_code
+
+
 
 @api.route('/rutas/ruta_mas_corta/<string:origen>/<string:destino>')
 class RutaMasCorta(Resource):
     @api.doc(description='Obtiene los caminos directos más cortos de una ruta en base a un origen y destino')
     def get(self, origen, destino):
-       response = get_ruta_mas_corta(origen, destino)
-       return jsonify(response)
+       response, status_code = get_ruta_mas_corta(origen, destino)
+       return response, status_code
 
 @api.route('/rutas/rutas_de_un_nodo/<string:nodo>')
 class AllRoutes(Resource):
     @api.doc(description='Obtiene todas las rutas de un nodo')
     def get(self, nodo):
-       response = get_rutas_de_un_nodo(nodo)
-       return jsonify(response)
+       response, status_code = get_rutas_de_un_nodo(nodo)
+       return response, status_code
 
 @api.route('/rutas/ruta_individual/<string:ruta>')
 class RutaIndividual(Resource):
     @api.doc(description='Obtiene información de una ruta individual')
-    def get(self, ruta):     
-        response = get_route(ruta)
-        return jsonify(response)
+    def get(self, ruta):
+        response, status_code = get_route(ruta)
+        return response, status_code
+
 
 
 
--- connection.py
+++ connection.py
@@ -1,5 +1,7 @@
 from flask_restx import Api
+from flask import jsonify
 from py2neo import Graph
+from neo4j.exceptions import Neo4jError, ClientError
 
 
 graph = Graph("neo4j+s://45486069.databases.neo4j.io", auth=("neo4j", "12345678"))
@@ -7,60 +9,95 @@ api = Api()
 
 
 def get_all_nodes():
-    response = graph.run("""MATCH (n)
-        RETURN collect(n.nombre) AS nodos""").data()
-    return response
+    try:
+        response = graph.run("""MATCH (n)
+            RETURN collect(n.nombre) AS nodos""").data()
+        return response, 200
+    except Neo4jError as e:
+        error_message = str(e)
+        return {'error': error_message}, 500
 
 def get_route(ruta):
-    query = """MATCH (n:Parada)-[r:""" + ruta + """]->(m:Parada)
-        WITH COLLECT(n) + COLLECT(m) AS nodos, r
-        UNWIND nodos AS nodo
-        WITH collect(DISTINCT CASE WHEN r.tipo = "ida" THEN nodo.nombre ELSE null END) AS ida, collect(DISTINCT CASE WHEN r.tipo = "vuelta" THEN nodo.nombre ELSE null END) AS vuelta
-        RETURN { ida: ida, vuelta: vuelta } AS camino"""
-    response = graph.run(query).data()
-    return response
+    try:
+        query = """MATCH (n:Parada)-[r:""" + ruta + """]->(m:Parada)
+            WITH COLLECT(n) + COLLECT(m) AS nodos, r
+            UNWIND nodos AS nodo
+            WITH collect(DISTINCT CASE WHEN r.tipo = "ida" THEN nodo.nombre ELSE null END) AS ida, collect(DISTINCT CASE WHEN r.tipo = "vuelta" THEN nodo.nombre ELSE null END) AS vuelta
+            RETURN { ida: ida, vuelta: vuelta } AS camino"""
+        response = graph.run(query).data()
+
+        if not response or not response[0]['camino']['ida']:
+            error_message = "La ruta no existe"
+            return {'error': error_message}, 404
+
+        return response, 200
+    
+    except Neo4jError as e:
+        error_message = str(e)
+        return {'error': error_message}, 500
 
 def get_rutas_de_un_nodo(nodo):
-    query = """ MATCH (n:Parada {nombre: '""" +nodo+ """'})-[r]-(m:Parada)
-        WITH collect({coste: r.coste, ruta: type(r), tipo: r.tipo}) AS rutas
-        RETURN rutas"""
-    result = graph.run(query).data()
-    return result
+    try:
+        query = """ MATCH (n:Parada {nombre: '""" +nodo+ """'})-[r]-(m:Parada)
+            WITH collect({coste: r.coste, ruta: type(r), tipo: r.tipo}) AS rutas
+            RETURN rutas"""
+        response = graph.run(query).data()
+        
+    
+        if not response or not response[0]['rutas']:
+            error_message = "El nodo ingresado no existe"
+            return {'error': error_message}, 404
+        
+        return response, 200
+    
+    except Neo4jError as e:
+        error_message = str(e)
+        return {'error': error_message}, 500
 
 def get_ruta_mas_corta(origen, destino):
-    query = """MATCH (origen:Parada {nombre: $origen}), (destino:Parada {nombre: $destino})
-        WITH origen, destino, ['ruta_100', 'ruta_101', 'ruta_214', 'ruta_313', 'ruta_316', 'ruta_561', 'ruta_562'] AS rutas
-        UNWIND rutas AS ruta
-        CALL apoc.cypher.run('
-        MATCH path = (o:Parada {nombre: $origen})-[*1..50]->(d:Parada {nombre: $destino})
-        WHERE ALL(rel in relationships(path) WHERE type(rel) = $ruta)
-        RETURN [node in nodes(path) | node.nombre] AS camino, $ruta AS ruta, length(path) AS longitud
-        ORDER BY longitud
-        LIMIT 1
-        ', {origen: origen.nombre, destino: destino.nombre, ruta: ruta}) YIELD value
-        RETURN value.camino AS camino, value.ruta AS ruta, value.longitud AS longitud"""
+
+    try: 
+        query = """MATCH (origen:Parada {nombre: $origen}), (destino:Parada {nombre: $destino})
+            WITH origen, destino, ['ruta_100', 'ruta_101', 'ruta_214', 'ruta_313', 'ruta_316', 'ruta_561', 'ruta_562'] AS rutas
+            UNWIND rutas AS ruta
+            CALL apoc.cypher.run('
+            MATCH path = (o:Parada {nombre: $origen})-[*1..50]->(d:Parada {nombre: $destino})
+            WHERE ALL(rel in relationships(path) WHERE type(rel) = $ruta)
+            RETURN [node in nodes(path) | node.nombre] AS camino, $ruta AS ruta, length(path) AS longitud
+            ORDER BY longitud
+            LIMIT 1
+            ', {origen: origen.nombre, destino: destino.nombre, ruta: ruta}) YIELD value
+            RETURN value.camino AS camino, value.ruta AS ruta, value.longitud AS longitud"""
+            
+        result = graph.run(query, origen=origen, destino=destino).data()
         
-    result = graph.run(query, origen=origen, destino=destino).data()
-    
-    resultados_procesados = []
-    
-    for r in result:
-        camino = r['camino']
-        ruta = r['ruta']
-        longitud = r['longitud']
+        resultados_procesados = []
         
-        resultado = {
-            'camino': camino,
-            'ruta': ruta,
-            'longitud': longitud
+        for r in result:
+            camino = r['camino']
+            ruta = r['ruta']
+            longitud = r['longitud']
+            
+            resultado = {
+                'camino': camino,
+                'ruta': ruta,
+                'longitud': longitud
+            }
+            
+            resultados_procesados.append(resultado)
+        
+        response = {
+            'origen': origen,
+            'destino': destino,
+            'resultados': resultados_procesados
         }
         
-        resultados_procesados.append(resultado)
+        if not response or not response['resultados']:
+            error_message = "El nodo origen o destino no existe"
+            return {'error': error_message}, 404
+
+        return response, 200
     
-    response = {
-        'origen': origen,
-        'destino': destino,
-        'resultados': resultados_procesados
-    }
-    
-    return response
+    except Neo4jError as e:
+        error_message = str(e)
+        return {'error': error_message}, 500
